c215d19758fe381dd7c753c9c97023dcd0ee98af
Vitor Henrique <vitorhenrique018@gmail.com>
2018-04-25 09:27:57 -0300

    Aplicativo Pronto

---
+++ topico-3/App de Ordenacao de Letras.js
@@ -0,0 +1,13 @@
+function OrdernaLetras (palavra) { 
+  
+    while(palavra.indexOf(" ") != -1)  // usei esse comando para tirar o espaço em branco
+    palavra = palavra.replace(" ", "");
+
+    var string = palavra.split(""); // aqui eu separo letra por letra, divide o objeto string em um array com substrings
+
+     var final = string.sort();   //aqui eu ordenei a string em ordem alfabetica ou seja ordena os elementos do array
+
+     return final.join("");     // Junto todos os elementos do array e com "" se a palavra tiver espaco ela irá junta-las
+} 
+
+console.log(OrdernaLetras("vitor henrique antonio da silva")); 
