e2b87dce9fcfd4d53be4d928e36444f170296642
Vitor Henrique <vitorhenrique018@gmail.com>
2018-04-27 17:23:39 -0300

    Testes Incluidos + via Browser e falta a documentacao

--- topico-3/test/test-App.js
+++ topico-3/test/test-App.js
@@ -15,4 +15,24 @@ QUnit.test("Ordenacao Normal", function (assert) {
 
   // Verifica se o resultado produzido é o esperado.
   assert.equal(resultado,"iortv" ,"Ordenacao Errada");
-});
+})
+;
+
+QUnit.test("Uma Letra", function (assert) {
+
+  // Executa a operação que desejamos testar
+  let resultado2 = operacao.OrdenaLetras("a");
+
+  // Verifica se o resultado produzido é o esperado.
+  assert.equal(resultado2,"a" ,"Ordenacao Errada");
+})
+;
+QUnit.test("Ordenacao Normal", function (assert) {
+
+  // Executa a operação que desejamos testar
+  let resultado3 = operacao.OrdenaLetras("qwertyuiopasdfghjklzxcvbnm");
+
+  // Verifica se o resultado produzido é o esperado.
+  assert.equal(resultado3,"abcdefghijklmnopqrstuvwxyz" ,"Ordenacao Errada");
+})
+;
